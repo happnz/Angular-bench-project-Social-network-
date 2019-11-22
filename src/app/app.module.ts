70f8fd3c318f1a3360a425f655a79f2e2d4496ee
Евгений Петрович <evgen280998@gmail.com>
2019-11-22 18:36:51 +0600

    sign-up form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [{path: '', component: AppComponent},
+  {path: 'sign-up', component: SignUpComponent}];
 
 @NgModule({
   declarations: [
@@ -12,7 +17,9 @@ import {FormsModule} from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../user';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {Router} from '@angular/router';
+import {catchError, tap} from 'rxjs/operators';
+import {of} from 'rxjs';
+
+enum FormStatus {
+  NOT_SUBMITTED,
+  SENDING,
+  SUBMITTED,
+  FAILED
+}
 
 @Component({
   selector: 'app-sign-up',
@@ -8,14 +20,38 @@ import {User} from '../user';
 })
 export class SignUpComponent implements OnInit {
   model: User;
+  apiUrl = environment.apiUrl;
+  path = 'sign-up';
+  status: FormStatus = FormStatus.NOT_SUBMITTED;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.model = new User('', '', '', '');
   }
 
   onSubmit() {
-    // TODO
+    this.status = FormStatus.SENDING;
+    const headers = new HttpHeaders({ 'Content-Type' : 'application/json'});
+    this.http.post<User>(`${this.apiUrl}/${this.path}`, this.model, {headers})
+      .pipe(
+        catchError(err => {
+          this.status = FormStatus.FAILED;
+          return of(null);
+        })
+      )
+      .subscribe(() => {
+        if (this.status !== FormStatus.FAILED) {
+          this.status = FormStatus.SUBMITTED;
+          this.router.navigate(['/']);
+        }
+      });
+  }
+
+  get FormStatus() {
+    return FormStatus;
   }
 }
