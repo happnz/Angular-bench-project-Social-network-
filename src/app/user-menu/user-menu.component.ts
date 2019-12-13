24566d1f0a293c878625bc9888e1c514f017e4cb
epetrovich <epetrovich@lineate.com>
2019-12-13 16:32:56 +0600

    user menu component

--- src/app/user-menu/user-menu.component.ts
+++ src/app/user-menu/user-menu.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-user-menu',
   templateUrl: './user-menu.component.html',
-  styleUrls: ['./user-menu.component.css']
+  styleUrls: ['./user-menu.component.scss']
 })
-export class UserMenuComponent implements OnInit {
+export class UserMenuComponent implements OnInit, OnDestroy {
+  private additionalOffset = 50;
+  scrolledDown = false;
+  componentScrollHandler = this.scrollHandler.bind(this);
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    window.addEventListener('scroll', this.componentScrollHandler);
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.componentScrollHandler);
+  }
+
+  private scrollHandler() {
+    const menuElem: HTMLElement = document.querySelector('.user-menu-holder');
+
+    this.scrolledDown = window.pageYOffset > menuElem.offsetTop + menuElem.offsetHeight + this.additionalOffset;
+  }
 }
