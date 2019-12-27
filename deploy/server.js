5404fcaf9365d9c6b9d89bcd8c110c0965999575
epetrovich <epetrovich@lineate.com>
2019-12-27 14:50:00 +0600

    wip

--- deploy/server.js
+++ deploy/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(express.static('./dist/social-frontend'));
+app.use(express.static('./social-frontend'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname,'/dist/social-frontend/index.html'));
+  res.sendFile(path.join(__dirname,'/social-frontend/index.html'));
 });
 
 const port = process.env.PORT || 8080;
