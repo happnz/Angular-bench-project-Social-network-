88c40263e9324fbad9825339a0b9f3fe805f3491
Евгений Петрович <evgen280998@gmail.com>
2019-12-29 21:41:20 +0600

    fix prod issue by removing default exports (aot compiler which is used to build production bundle does not support default exports)

--- src/app/logged-out-guard.ts
+++ src/app/logged-out-guard.ts
@@ -1,11 +1,10 @@
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import {Injectable} from '@angular/core';
 import {SessionQuery} from './state/session/session.query';
-import {Observable, of} from 'rxjs';
 import {take} from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
-export default class LoggedOutGuard implements CanActivate {
+export class LoggedOutGuard implements CanActivate {
 
   constructor(private router: Router,
               private sessionQuery: SessionQuery) {
